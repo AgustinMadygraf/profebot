5f14799e80cc77ffaba705dae96741ec956492d7
AgustinMadygraf <agustin.mtto.madygraf>
2025-03-13 01:21:19 -0300

    Enhance simple logger; implement colored output and format fields for improved readability

--- src/utils/logging/simple_logger.py
+++ src/utils/logging/simple_logger.py
@@ -4,11 +4,15 @@ Path: src/utils/logging/simple_logger.py
 
 import sys
 import logging
+import colorlog
 
 def get_logger() -> logging.Logger:
     """
     Obtiene un logger simple para la aplicación,
-    configurando su nivel según los argumentos de la línea de comandos.
+    configurando su nivel según los argumentos de la línea de comandos
+    y utilizando colores en consola. Los campos 'filename' y 'levelname'
+    se formatean para tener siempre 15 y 5 caracteres respectivamente, y el
+    número de línea se reserva en 3 dígitos con relleno de ceros.
     """
     logger = logging.getLogger("app_logger")
 
@@ -20,8 +24,17 @@ def get_logger() -> logging.Logger:
 
     # Verifica si ya existen handlers para evitar duplicaciones
     if not logger.handlers:
-        formatter = logging.Formatter("%(filename)s:%(lineno)d - %(levelname)s - %(message)s")
         console_handler = logging.StreamHandler()
+        formatter = colorlog.ColoredFormatter(
+            "%(log_color)s%(filename)15.15s:%(lineno)03d - %(levelname)-5.5s - %(message)s",
+            log_colors={
+                'DEBUG':    'cyan',
+                'INFO':     'green',
+                'WARNING':  'yellow',
+                'ERROR':    'red',
+                'CRITICAL': 'red,bg_white',
+            }
+        )
         console_handler.setFormatter(formatter)
         logger.addHandler(console_handler)
 
